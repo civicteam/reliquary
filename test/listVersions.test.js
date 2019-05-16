b302260f654bef9ea93b6427f420dd597713a7c5
Daniel Augusto de Melo Santos <danielmelos@gmail.com>
2019-05-16 12:13:41 -0300

    Removing reference to any Civic info

--- test/listVersions.test.js
+++ test/listVersions.test.js
@@ -13,9 +13,9 @@ describe('Tests for the list versions methods', () => {
           CreatedDate: '2019 - 05 - 13 T13: 36: 40.566 Z',
         },
         ],
-        NextToken: 'AAGWzGu7c3fnjy994XFY80EKjACAkQA3jPXB5sokoh0+PEQgQ+UY7OiiU0yt2WIfHhNLenRUW7cy1X+okG6qkgsEBvqO3kxmm7tzq+awLyWUPVF4wbmXVYdxYHXyiMMzGlR884DfhX3uYJU35BAAAXTkB6pAu5c2U71zGAskNco34Ev1O7GBIPqV56qjBXpWp1n6AAIUDPxgdfz2CkzbN21ZPO48aHkA4OTAyZmriB26wTerot72pgLSS6ze+3RZzk0BdFU/jmXO54g3b/GQetQKBD2X2CCvUfu6zo92gaar',
-        ARN: 'arn:aws:secretsmanager:us-east-1:249634870252:secret:reliquary-test-GRIQNJ',
-        Name: 'reliquary-test',
+        NextToken: 'dummy',
+        ARN: 'arn:aws:secretsmanager:us-east-1:99999999999:secret:dummy',
+        Name: 'dummy',
       });
     });
   });
